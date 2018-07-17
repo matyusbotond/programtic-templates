684f6ec2bd0ab28b88653d2d78143cbf668c5066
Mátyus Botond <matyusbotond@outlook.hu>
2018-07-17 09:27:24 +0200

    Store alapok

---
+++ Generics/Generics/Store.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Store
+    {
+        object[] _innerStore;
+        int _currentIndex;
+
+        public Store()
+        {
+            _innerStore = new object[5];
+        }
+
+        public void Add(object item)
+        {
+            _innerStore[_currentIndex] = item;
+            _currentIndex++;
+        }
+
+        public object Get(int index)
+        {
+            if (index >= _currentIndex)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return _innerStore[index];
+        }
+    }
+}
